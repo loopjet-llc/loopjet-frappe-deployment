6411cc27ecd19e28e18deb3d0e69788931b398db
Ahmad El-Ali <ahmad@el-ali.de>
2026-07-11 19:03:48 +0800

    fix: align local image architecture with runtime

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -8,12 +8,14 @@ cd "$ROOT"
 [[ -f .env ]] && set -a && source .env && set +a
 CUSTOM_IMAGE=${CUSTOM_IMAGE:-ghcr.io/loopjet-llc/loopjet-frappe-suite}
 CUSTOM_TAG=${CUSTOM_TAG:-local}
+BUILD_PLATFORM=${BUILD_PLATFORM:-linux/amd64}
 
 RUNTIME=$(./scripts/bootstrap-frappe-docker.sh)
 ./scripts/generate-apps.py
 FRAPPE_REF=$(python3 -c 'import json; print(json.load(open("config/versions.json"))["frappe"]["ref"])')
 
 docker build \
+  --platform "$BUILD_PLATFORM" \
   --build-arg "FRAPPE_PATH=https://github.com/loopjet-llc/loopjet-frappe.git" \
   --build-arg "FRAPPE_BRANCH=$FRAPPE_REF" \
   --secret "id=apps_json,src=$ROOT/config/apps.generated.json" \
