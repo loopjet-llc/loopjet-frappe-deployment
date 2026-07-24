15f983f60cb467668a01eb110b7b5a51b5947ac5
Ahmad El-Ali <ahmad@el-ali.de>
2026-07-24 10:55:46 +0800

    Deploy Loopjet custom v0.1.18

--- tests/test_config.py
+++ tests/test_config.py
@@ -36,7 +36,7 @@ class ConfigurationTest(unittest.TestCase):
 		self.assertIn(
 			{
 				"url": "https://github.com/loopjet-llc/loopjet-frappe-custom.git",
-				"branch": "v0.1.17",
+				"branch": "v0.1.18",
 			},
 			upstream_apps,
 		)
