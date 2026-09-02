9f7b21afa2e1840ee9e59d67a196ea004c20872b
Ahmad El-Ali <ahmad@el-ali.de>
2026-09-02 13:39:13 +0800

    deploy: publish email brand isolation

--- tests/test_config.py
+++ tests/test_config.py
@@ -43,7 +43,7 @@ class ConfigurationTest(unittest.TestCase):
 		self.assertIn(
 			{
 				"url": "https://github.com/loopjet-llc/loopjet-frappe-custom.git",
-				"branch": "v0.3.5",
+				"branch": "v0.3.6",
 			},
 			upstream_apps,
 		)
