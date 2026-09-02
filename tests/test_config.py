1a79b8bd735f0e8fe7f14c0875abf691dba9fb9c
Ahmad El-Ali <ahmad@el-ali.de>
2026-09-02 18:14:10 +0800

    deploy: publish Founder Cockpit

--- tests/test_config.py
+++ tests/test_config.py
@@ -43,7 +43,7 @@ class ConfigurationTest(unittest.TestCase):
 		self.assertIn(
 			{
 				"url": "https://github.com/loopjet-llc/loopjet-frappe-custom.git",
-				"branch": "v0.3.7",
+				"branch": "v0.4.0",
 			},
 			upstream_apps,
 		)
