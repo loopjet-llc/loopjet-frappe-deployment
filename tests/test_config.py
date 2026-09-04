81e2400f8ae313015a187c50e8398b1dcf4d1e98
Ahmad El-Ali <ahmad@el-ali.de>
2026-09-04 15:58:31 +0800

    deploy: publish light CI invoice template

--- tests/test_config.py
+++ tests/test_config.py
@@ -43,7 +43,7 @@ class ConfigurationTest(unittest.TestCase):
 		self.assertIn(
 			{
 				"url": "https://github.com/loopjet-llc/loopjet-frappe-custom.git",
-				"branch": "v0.4.1",
+				"branch": "v0.4.2",
 			},
 			upstream_apps,
 		)
